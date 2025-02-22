a25af7edbcb02a4129fdddf2082623176ad27068
jgur-psyops <jonathan@mrgn.group>
2025-02-22 01:48:07 -0500

    Comment ixes and add to processor

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -1388,6 +1388,14 @@ impl Processor {
                 msg!("Instruction: InitializePool");
                 Self::process_initialize_pool(program_id, accounts)
             }
+            SinglePoolInstruction::InitializeTempStake => {
+                msg!("Instruction: InitializeTempPool");
+                Self::process_initialize_temp_stake(program_id, accounts)
+            }
+            SinglePoolInstruction::ProcessMergeTempStake => {
+                msg!("Instruction: MergeTempStake");
+                Self::process_merge_temp_stake(program_id, accounts)
+            }
             SinglePoolInstruction::ReactivatePoolStake => {
                 msg!("Instruction: ReactivatePoolStake");
                 Self::process_reactivate_pool_stake(program_id, accounts)
