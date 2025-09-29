8d09bbd1ce1b9626a53d2e9026791864f57ab975
Jon C <me@jonc.dev>
2025-09-29 17:49:12 +0200

    CI: Move repo to a Makefile
    #### Problem
    
    The Makefile model works, but the single-pool repo is still using
    package.json commands.
    
    #### Summary of changes
    
    Similar to a few other repos:
    
    * Remove the old commands in package.json
    * Remove the old setup script
    * Replace all workflows with the reusable versions
    * Update tracing-subscriber for audit issue
    * Remove test-sbf feature
    * Fix spelling mistakes
    * Fix a clippy failure exposed from removing test-sbf

--- scripts/ci/set-env.mjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zx
-import { getSolanaVersion, getToolchain } from '../utils.mjs';
-
-await $`echo "SOLANA_VERSION=${getSolanaVersion()}" >> $GITHUB_ENV`;
-await $`echo "TOOLCHAIN_NIGHTLY=${getToolchain('nightly')}" >> $GITHUB_ENV`;
--- scripts/rust/hack.mjs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zx
-import 'zx/globals';
-import { cliArguments, getToolchainArgument } from '../utils.mjs';
-
-const toolchain = getToolchainArgument('nightly');
-await $`cargo ${toolchain} hack check --all-targets --feature-powerset ${cliArguments()}`;
