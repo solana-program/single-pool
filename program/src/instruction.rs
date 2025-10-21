19dac6f287e60ba4e66f74b6ea3c7855e7f814f6
hana <81144685+2501babe@users.noreply.github.com>
2025-10-21 07:06:02 -0700

    improve deprecation message

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -421,7 +421,10 @@ pub fn withdraw_stake(
 /// lockup.
 #[deprecated(
     since = "3.0.0",
-    note = "Will be removed in a future release; this was intended to support a wallet flow that never materialized."
+    note = "Default deposit helpers will be removed in a future release; these were \
+    intended to support a wallet flow that never materialized. To set up a new stake \
+    account for deposit, use `instruction::create_account_and_delegate_stake` from \
+    `solana-stake-interface` using any normal keypair."
 )]
 pub fn create_and_delegate_user_stake(
     program_id: &Pubkey,
